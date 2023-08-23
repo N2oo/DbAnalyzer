d8a3a6acd871e955a3fb9aa9c0e4fdc68df051c3
N2oo <44746112+N2oo@users.noreply.github.com>
2023-08-23 17:08:32 +0200

    Modification des ordre d'affichages dans les formulaires

--- src/Form/FieldType.php
+++ src/Form/FieldType.php
@@ -6,6 +6,7 @@ use App\Entity\Field;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Repository\FieldRepository;
 
 class FieldType extends AbstractType
 {
@@ -16,7 +17,14 @@ class FieldType extends AbstractType
             ->add('length')
             ->add('forTable')
             ->add('type')
-            ->add('useProperty')
+            ->add('useProperty',null,[
+                'query_builder' => function (FieldRepository $er) {
+                    return $er->createQueryBuilder('f')
+                        ->leftjoin('f.forTable', 't')
+                        ->addOrderBy('t.name','ASC')
+                        ->addOrderBy('f.name','ASC');
+                },
+            ])
             ->add('isPrimary')
             ->add('field_original_id')
             ->add('commentary')
