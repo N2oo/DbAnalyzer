db34ba86d4314114e27851231c4ed9165794f32e
N2oo <44746112+N2oo@users.noreply.github.com>
2024-02-06 17:32:54 +0100

    Modifications mineures

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ColumnRepository;
+use App\Repository\TableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    public function __construct(
+        private ColumnRepository $repo
+    )
+    {
+        $result = $repo->find("1");
+        dump($result);
+
+    }
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
