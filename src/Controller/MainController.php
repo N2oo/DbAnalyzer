f691d85c687b0ddf4532c950a05ad1bb5d6191e0
N2oo <44746112+N2oo@users.noreply.github.com>
2024-02-08 09:25:12 +0100

    Suppression de templates + controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,20 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\ColumnRepository;
-use App\Repository\TableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    public function __construct(
-        private ColumnRepository $repo
-    )
+    public function __construct()
     {
-        $result = $repo->find("1");
-        dump($result);
 
     }
     #[Route('/', name: 'app_main')]
