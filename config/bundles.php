f0a8adfd0bb4b15817cb11d98040dd56f1393b17
N2oo <44746112+N2oo@users.noreply.github.com>
2024-02-08 15:30:20 +0100

    Mise en place du bundle Auto-complete

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
