0b9295be827e6f4949b83ba8d3db6f6d7840f847
N2oo <44746112+N2oo@users.noreply.github.com>
2024-02-14 15:49:36 +0100

    ajout du clear de cache

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,3 +1,8 @@
 php bin/console d:m:m --no-interaction
+php bin/console cache:clear --env=dev
+php bin/console cache:clear --env=prod
+php bin/console cache:warmup --env=dev
+php bin/console cache:warmup --env=prod
+
 exec apache2-foreground
 # Jouer les migrations ici
