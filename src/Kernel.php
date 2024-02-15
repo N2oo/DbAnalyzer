a99cf704b6da7c48c578fe3c182784060ecc6484
N2oo <44746112+N2oo@users.noreply.github.com>
2024-02-15 13:28:12 +0100

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,9 +2,7 @@
 
 namespace App;
 
-use App\DependencyInjection\Compiler\MultipleEntityProcessorInterfaceDecoratorPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
